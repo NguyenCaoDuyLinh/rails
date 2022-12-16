332cf92e2fe224e58adc1ddd4d2685f7d3a5d732
NguyenCaoDuyLinh <linhgood04@gmail.com>
2022-12-16 17:49:06 +0700

    first22222 commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+    def new
+        @student = Student.new
+        @students = Student.all 
+    end
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to new_student_path
+        end
+
+    end
+
+    private
+    def student_params
+        params.require(:student).permit(:msv, :hoten)
+    end
+end
